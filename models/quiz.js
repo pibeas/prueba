980ef91a7e57bb8e7a621daf2bac7220b47f6ac8
pibeas <pedroitrabajo@gmail.com>
2015-08-20 17:15:14 +0200

    autologout

--- models/quiz.js
+++ models/quiz.js
@@ -1,17 +1,17 @@
-// Definicion del modelo de Quiz con validación
+
 
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define(
     'Quiz',
     { pregunta: {
         type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+        validate: { notEmpty: {msg: "-> Falta pregunta"}}
       },
       respuesta: {
         type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+        validate: { notEmpty: {msg: "-> Falta respuesta"}}
       },
-      image: {
+      tema: {
         type: DataTypes.STRING
       }
     }
